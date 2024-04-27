86719e8cea31fbfcc13cd16543933333ccb8098d
Fernando <fsuarez15@alumnos.uaq.mx>
2024-04-26 23:20:30 -0600

    nombre hola mundo

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = 3000
 app.get('/',(req,res)=>{
     res.send("Hola Mundo")
 })
+app.get('/:nombre',(req,res)=>{
+    res.send("Hola Mundo " + req.params.nombre)
+})
 app.listen(port,()=>{
     console.log('Aplicacion Corriendo')
 })
